3f7a9ede964911403a9ab3eccd9e987816b4248e
Jonathan Hackworth <jh@j22h.com>
2019-09-17 13:35:19 -0700

    styled final

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Vue + Tailwind Boilerplate
 This repo is meant to be a repository for a tutorial on how to use custom fonts with Tailwind CSS. See j22h.com for the full tutorial.
 
+Final project looks like the image below:
+![Final Image](https://github.com/jhackworth/tailwind-custom-font-tutorial/blob/master/src/assets/img/final.png)
+
 ## Project setup
 ```
 yarn install
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
 <template>
-  <div class="pt-24 text-center">
-		<h1 class="text-4xl font-dm-serif">A Luxurious Headline</h1>
-		<p class="font-inter">Supporting text goes here</p>
-	</div>
+  <div class="container mx-auto mt-12 px-5">
+    <div class="text-center bg-red-200 py-2 mx-2 my-3 rounded xl:hidden">
+      <p>
+        This page wasn't setup to be responsive and probably looks weird at this resolution — sorry!
+      </p>
+    </div>
+
+    <div class="flex flex-wrap justify-start">
+      <div class="h-56 w-40 bg-yellow-500"></div>
+
+      <div class="pt-8 -ml-32">
+        <h1 class="text-6xl font-dm-serif">A Luxurious Headline</h1>
+        <p class="text-sm font-inter uppercase tracking-wide ml-32 pl-2 italic -mt-2">Supporting text goes here</p>
+      </div>
+    </div>
+
+    <div class="flex flex-row items-start mb-24">
+      <div class="mt-10 pl-4 pr-16 w-3/5">
+        <h1 class="font-dm-serif text-3xl">Subheadline here</h1>
+        <p class="font-inter">
+          Curabitur dictum metus et orci tristique pharetra. Sed non eleifend lacus. In fringilla nunc quis gravida iaculis. Duis facilisis sollicitudin turpis ac dignissim. Nulla gravida libero vel eros iaculis ultrices. Aenean eros justo, gravida sed libero id, ornare placerat eros. Nullam condimentum metus ut quam tincidunt, sit amet mattis nisi dapibus. Sed leo diam, commodo eu magna eu, facilisis tempor velit. Sed imperdiet lectus libero, dapibus blandit velit sollicitudin sit amet.
+        </p>
+        <p class="font-inter mt-5">  
+          Vestibulum sit amet dictum lorem. Pellentesque vehicula convallis sapien ut interdum. Nullam est ipsum, euismod vel tortor ac, gravida placerat est. Nullam auctor nec nibh accumsan mattis. Aenean purus quam, interdum sit amet orci sit amet, cursus tincidunt ex. Nam consectetur tortor nec auctor maximus. Nullam euismod lorem rhoncus justo consequat blandit. Sed sagittis bibendum dolor sollicitudin volutpat. Duis semper vulputate mauris sit amet eleifend. Donec euismod purus non lacus fringilla molestie.
+        </p>
+        <p class="font-inter mt-5">  
+          Praesent id metus gravida, cursus justo a, sodales nulla. In venenatis tincidunt lorem, a vestibulum enim mollis a. Nullam sit amet fermentum ex. Ut imperdiet, arcu ut placerat pulvinar, est nisl imperdiet tortor, eget bibendum tellus dolor in ipsum. Nunc turpis urna, auctor sed ullamcorper sit amet, pretium et quam. Donec elementum nulla eget tempus aliquam. Suspendisse mollis enim justo, vel sagittis massa euismod eu. Pellentesque porta maximus justo, vitae semper ante ultrices non. Phasellus pulvinar ligula in est pellentesque, nec ullamcorper quam porta. Integer nec turpis ac magna finibus placerat. Ut at tempus orci.
+        </p>
+      </div>
+
+      <div class="w-2/5">
+        <img src="./assets/img/fashion.jpg" class="img -mt-5">
+        <p class="text-xs font-inter font-medium italic text-right">Photo by <a class="underline" href="https://unsplash.com/@ohthehumanity" target="_blank">Dom Hill</a> on <a class="underline" href="https://unsplash.com" target="_blank">Unsplash</a></p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,5 +47,7 @@ export default {
 </script>
 
 <style>
-
+.img {
+  box-shadow: 7px 7px 0px 0px #ecc94b;
+}
 </style>
