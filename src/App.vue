69dcb1d2573c5982d93ece1e24750fe421474a5c
Jonathan Hackworth <jh@j22h.com>
2019-09-17 12:28:29 -0700

    add local font inter

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pt-24 text-center">
 		<h1 class="text-4xl font-dm-serif">A Luxurious Headline</h1>
-		<p>Supporting text goes here</p>
+		<p class="font-inter">Supporting text goes here</p>
 	</div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     extend: {
       fontFamily: {
         'dm-serif': ['DM Serif Display', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+        'inter': ['Inter', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
       }
     }
   },
